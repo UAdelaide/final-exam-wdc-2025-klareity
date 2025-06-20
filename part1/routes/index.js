00eadc16cf0157062b779d2f8fabcff08c365edd
klareity <taonanite2014@gmail.com>
2025-06-20 15:29:47 +0000

    [automatic commit] 2025-06-20 15:29:47 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql12/promise');
 
+const 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/dogs', async (req, res, next) {
+router.get('/api/dogs', async (req, res, next) => {
 
 });
 
