88820d8f346ae35c75074be3bcea13c90da04a51
klareity <taonanite2014@gmail.com>
2025-06-21 06:21:14 +0000

    [automatic commit] 2025-06-21 06:21:14 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,7 +70,7 @@ router.post('/logout', async (req, res) => {
       return res.status(500).json({ error: 'Logout failed' });
     }
     res.clearCookie('connect.sid');
-    res.json({ message: ''})
+    res.json({ message: 'Logged out'});
   });
 
 });
