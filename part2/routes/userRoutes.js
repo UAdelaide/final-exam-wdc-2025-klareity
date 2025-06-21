93783644706c489e6a34ff3fd422b78bb58d6b66
klareity <taonanite2014@gmail.com>
2025-06-21 06:17:48 +0000

    [automatic commit] 2025-06-21 06:17:48 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', async (req, res) => {
   req.session.destroy(err => {
-    
+    res.clearCookie()
   })
   res.status(500).json({ error: 'Login failed' });
 });
