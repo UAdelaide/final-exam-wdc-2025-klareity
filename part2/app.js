76576906ebd94e2114deb3d13a3cc166cd3402f5
klareity <taonanite2014@gmail.com>
2025-06-21 05:57:12 +0000

    [automatic commit] 2025-06-21 05:57:12 UTC

--- part2/app.js
+++ part2/app.js
@@ -9,8 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    secret: ''
-}))
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
